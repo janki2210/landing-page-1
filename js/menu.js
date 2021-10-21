a4fc18fbb6db197d09e99d4098eaad59867828e0
janki2210 <80520217+janki2210@users.noreply.github.com>
2021-10-21 18:37:47 +0530

    main

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+     var menutoggle = document.querySelector('.toggle');
+     var navigation = document.querySelector('.navigation');
+     menutoggle.classList.toggle('active')
+     navigation.classList.toggle('active')
+}
